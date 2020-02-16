44b25ce087c801ef7cd3767da7c7eb2797c8814e
Tag <tagadvance+github.com@gmail.com>
2020-02-15 18:58:13 -0700

    port WaitStrategies class

---
+++ src/tagadvance/elephant/retry/WaitStrategies.php
@@ -0,0 +1,343 @@
+<?php
+
+/*
+ * Copyright 2012-2015 Ray Holder
+ * Copyright 2020 Tag Spilman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace tagadvance\elephant\retry;
+
+use Respect\Validation\Validator;
+
+/**
+ * Factory class for instances of `WaitStrategy`.
+ *
+ * @see WaitStrategy
+ */
+final class WaitStrategies
+{
+
+	private function __construct()
+	{
+	}
+
+	/**
+	 * Returns a wait strategy that doesn't sleep at all before retrying. Use this at your own risk.
+	 *
+	 * @return WaitStrategy a wait strategy that doesn't wait between retries
+	 */
+	public static function noWait(): WaitStrategy
+	{
+		static $instance = null;
+		if ($instance === null) {
+			$instance = new FixedWaitStrategy();
+		}
+
+		return $instance;
+	}
+
+	/**
+	 * Returns a wait strategy that sleeps a fixed amount of time before retrying.
+	 *
+	 * @param float $sleepTimeSeconds the time to sleep
+	 * @return WaitStrategy a wait strategy that sleeps a fixed amount of time
+	 * @throws \InvalidArgumentException if `$sleepTimeSeconds` < 0
+	 */
+	public static function fixedWait(float $sleepTimeSeconds): WaitStrategy
+	{
+		return new FixedWaitStrategy($sleepTimeSeconds);
+	}
+
+	/**
+	 * Returns a strategy that sleeps a random amount of time before retrying.
+	 *
+	 * @param float $minimumTimeSeconds the minimum time to sleep
+	 * @param float $maximumTimeSeconds the maximum time to sleep
+	 * @return RandomWaitStrategy a wait strategy with a random wait time
+	 * @throws \InvalidArgumentException if `$minimumTimeSeconds` < 0
+	 * @throws \InvalidArgumentException if `$minimumTimeSeconds` >= `$maximumTimeSeconds`
+	 */
+	public static function randomWait(float $minimumTimeSeconds, float $maximumTimeSeconds): WaitStrategy
+	{
+		return new RandomWaitStrategy($minimumTimeSeconds, $maximumTimeSeconds);
+	}
+
+	/**
+	 * Returns a strategy that sleeps a fixed amount of time after the first failed attempt and in incrementing amounts
+	 * of time after each additional failed attempt.
+	 *
+	 * @param float $initialSleepTimeSeconds the time to sleep before retrying the first time
+	 * @param float $incrementSeconds the increment added to the previous sleep time after each failed attempt
+	 * @return WaitStrategy a wait strategy that incrementally sleeps an additional fixed time after each failed attempt
+	 * @throws \InvalidArgumentException if `$initialSleepTimeSeconds` < 0
+	 */
+	public static function incrementingWait(float $initialSleepTimeSeconds, float $incrementSeconds): WaitStrategy
+	{
+		return new IncrementingWaitStrategy($initialSleepTimeSeconds, $incrementSeconds);
+	}
+
+	/**
+	 * Returns a strategy which sleeps for an exponential amount of time after the first failed attempt, and in
+	 * exponentially incrementing amounts after each failed attempt up to the maximumTime.
+	 * The wait time between the retries can be controlled by the multiplier.
+	 *
+	 * @param float $multiplier multiply the wait time calculated by this
+	 * @param float $maximumTimeSeconds the maximum time to sleep
+	 * @return WaitStrategy a wait strategy that increments with each failed attempt using exponential backoff
+	 * @throws \InvalidArgumentException
+	 */
+	public static function exponentialWait(float $multiplier = 1, float $maximumTimeSeconds = PHP_FLOAT_MAX): WaitStrategy
+	{
+		return new ExponentialWaitStrategy($multiplier, $maximumTimeSeconds);
+	}
+
+
+	/**
+	 * Returns a strategy which sleeps for an increasing amount of time after the first failed attempt,
+	 * and in Fibonacci increments after each failed attempt up to the {@code maximumTime}.
+	 * The wait time between the retries can be controlled by the multiplier.
+	 * nextWaitTime = fibonacciIncrement * {@code multiplier}.
+	 *
+	 * @param float $multiplier multiply the wait time calculated by this
+	 * @param float $maximumTimeSeconds the maximum time to sleep
+	 * @return WaitStrategy a wait strategy that increments with each failed attempt using a Fibonacci sequence
+	 * @throws \InvalidArgumentException
+	 */
+	public static function fibonacciWait(float $multiplier = 1, float $maximumTimeSeconds = PHP_FLOAT_MAX): WaitStrategy
+	{
+		return new FibonacciWaitStrategy($multiplier, $maximumTimeSeconds);
+	}
+
+	/**
+	 * Returns a strategy which sleeps for an amount of time based on the Exception that occurred. The `$function`
+	 * determines how the sleep time should be calculated for the given `exceptionClass`. If the exception does not match, a wait time of 0 is returned.
+	 *
+	 * @param callable $function function to calculate sleep time
+	 * @param string $exceptionClass class to calculate sleep time from
+	 * @return WaitStrategy a wait strategy calculated from the failed attempt
+	 * @throws \InvalidArgumentException if `$exceptionClass` does not exist
+	 */
+	public static function exceptionWait(string $exceptionClass, callable $function): WaitStrategy
+	{
+		return new ExceptionWaitStrategy($exceptionClass, $function);
+	}
+
+	/**
+	 * Joins one or more wait strategies to derive a composite wait strategy.
+	 * The new joined strategy will have a wait time which is total of all wait times computed one after another in order.
+	 *
+	 * @param WaitStrategy ...$waitStrategies Wait strategies that need to be applied one after another for computing the sleep time.
+	 * @return CompositeWaitStrategy a composite wait strategy
+	 * @throws \InvalidArgumentException if `$waitStrategies` is empty
+	 */
+	public static function join(WaitStrategy...$waitStrategies): WaitStrategy
+	{
+		return new CompositeWaitStrategy(...$waitStrategies);
+	}
+}
+
+final class FixedWaitStrategy implements WaitStrategy
+{
+	private float $sleepTime;
+
+	public function __construct(float $sleepTime = 0)
+	{
+		Validator::min(0)->setName('$sleepTime')->check($sleepTime);
+
+		$this->sleepTime = $sleepTime;
+	}
+
+	public function computeSleepTime(Attempt $failedAttempt): float
+	{
+		return $this->sleepTime;
+	}
+}
+
+final class RandomWaitStrategy implements WaitStrategy
+{
+	private float $minimum;
+	private float $maximum;
+
+	public function __construct(float $minimum, float $maximum)
+	{
+		Validator::min(0)->setName('$minimum')->check($minimum);
+		$template = "\$maximum must be > $minimum but \$maximum is $maximum and \$minimum is $minimum";
+		Validator::callback(fn() => $maximum > $minimum)->setTemplate($template)->check(null);
+
+		$this->minimum = $minimum;
+		$this->maximum = $maximum;
+	}
+
+	public function computeSleepTime(Attempt $failedAttempt): float
+	{
+		$difference = $this->maximum - $this->minimum;
+		$random = (float)rand() / (float)getrandmax();
+
+		return $this->minimum + ($difference * $random);
+	}
+}
+
+final class IncrementingWaitStrategy implements WaitStrategy
+{
+	private float $initialSleepTime;
+	private float $increment;
+
+	public function __construct(float $initialSleepTime, float $increment)
+	{
+		Validator::min(0)->setName('$initialSleepTime')->check($initialSleepTime);
+
+		$this->initialSleepTime = $initialSleepTime;
+		$this->increment = $increment;
+	}
+
+	public function computeSleepTime(Attempt $failedAttempt): float
+	{
+		$result = $this->initialSleepTime + ($this->increment * ($failedAttempt->getAttemptNumber() - 1));
+		return $result >= 0 ? $result : 0;
+	}
+}
+
+final class ExponentialWaitStrategy implements WaitStrategy
+{
+	private float $multiplier;
+	private float $maximumWait;
+
+	public function __construct(float $multiplier, float $maximumWait)
+	{
+		Validator::min(0, false)->setName('$multiplier')->check($multiplier);
+		Validator::min(0)->setName('$maximumWait')->check($maximumWait);
+		Validator::callback(fn() => $multiplier < $maximumWait)->setName('$multiplier < $maximumWait')->check(null);
+
+		$this->multiplier = $multiplier;
+		$this->maximumWait = $maximumWait;
+	}
+
+	public function computeSleepTime(Attempt $failedAttempt): float
+	{
+		$attempt = $failedAttempt->getAttemptNumber();
+		if ($attempt === 1) {
+			return $this->multiplier;
+		}
+
+		$exp = pow(2, $attempt - 1);
+		$result = $this->multiplier * $exp;
+		// TODO: refactor to use min/max
+		if ($result > $this->maximumWait) {
+			$result = $this->maximumWait;
+		}
+
+		return $result >= 0 ? $result : 0;
+	}
+}
+
+final class FibonacciWaitStrategy implements WaitStrategy
+{
+	private float $multiplier;
+	private float $maximumWait;
+
+	public function __construct(float $multiplier, float $maximumWait)
+	{
+		Validator::min(0, false)->setName('$multiplier')->check($multiplier);
+		Validator::min(0)->setName('$maximumWait')->check($maximumWait);
+		Validator::callback(fn() => $multiplier < $maximumWait)->setName('$multiplier < $maximumWait')->check(null);
+
+		$this->multiplier = $multiplier;
+		$this->maximumWait = $maximumWait;
+	}
+
+	public function computeSleepTime(Attempt $failedAttempt): float
+	{
+		$fib = $this->fib($failedAttempt->getAttemptNumber());
+
+		$result = $this->multiplier * $fib;
+
+		// TODO: refactor to use min/max
+		if ($result > $this->maximumWait || $result < 0) {
+			$result = $this->maximumWait;
+		}
+
+		return $result >= 0 ? $result : 0;
+	}
+
+	// TODO: generator?
+	private function fib(int $n): float
+	{
+		if ($n === 0) {
+			return 0;
+		} else if ($n === 1) {
+			return 1;
+		};
+
+		$prevPrev = 0;
+		$prev = 1;
+		$result = 0;
+
+		for ($i = 2; $i <= $n; $i++) {
+			$result = $prev + $prevPrev;
+			$prevPrev = $prev;
+			$prev = $result;
+		}
+
+		return $result;
+	}
+}
+
+final class ExceptionWaitStrategy implements WaitStrategy
+{
+	private string $exceptionClass;
+	private $function;
+
+	public function __construct(string $exceptionClass, callable $function)
+	{
+		Validator::callback('class_exists')->setName('$exceptionClass')->check($exceptionClass);
+		// TODO: ensure of type \Throwable?
+
+		$this->exceptionClass = $exceptionClass;
+		$this->function = $function;
+	}
+
+	public function computeSleepTime(Attempt $lastAttempt): float
+	{
+		if ($lastAttempt->hasException()) {
+			$cause = $lastAttempt->getExceptionCause();
+			if ($cause instanceof $this->exceptionClass) {
+				return ($this->function)($cause);
+			}
+		}
+		return 0;
+	}
+}
+
+final class CompositeWaitStrategy implements WaitStrategy
+{
+	private array $waitStrategies;
+
+	public function __construct(WaitStrategy ...$waitStrategies)
+	{
+		Validator::notEmpty()->setName($waitStrategies)->check($waitStrategies);
+
+		$this->waitStrategies = $waitStrategies;
+	}
+
+	public function computeSleepTime(Attempt $failedAttempt): float
+	{
+		$waitTime = 0;
+		foreach ($this->waitStrategies as $waitStrategy) {
+			$waitTime += $waitStrategy->computeSleepTime($failedAttempt);
+		}
+
+		return $waitTime;
+	}
+}
---
+++ tests/unit/tagadvance/elephant/retry/WaitStrategiesTest.php
@@ -0,0 +1,254 @@
+<?php
+
+namespace tagadvance\elephant\retry;
+
+use PHPUnit\Framework\TestCase;
+
+class WaitStrategiesTest extends TestCase
+{
+	function testNoWait()
+	{
+		$expected = 0;
+
+		$attempt = \Mockery::mock(Attempt::class);
+
+		$waitStrategy = WaitStrategies::noWait();
+
+		$arbitrarilyLargeNumber = 100;
+		for ($i = 0; $i < $arbitrarilyLargeNumber; $i++) {
+			$sleepTime = $waitStrategy->computeSleepTime($attempt);
+			$this->assertEquals($expected, $sleepTime);
+		}
+	}
+
+	function testFixedWaitMinimumAtLeastZero()
+	{
+		$this->expectException(\InvalidArgumentException::class);
+
+		WaitStrategies::fixedWait(-1);
+	}
+
+	function testFixedWait()
+	{
+		$expected = 1;
+
+		$attempt = \Mockery::mock(Attempt::class);
+
+		$waitStrategy = WaitStrategies::fixedWait(1);
+
+		$arbitrarilyLargeNumber = 100;
+		for ($i = 0; $i < $arbitrarilyLargeNumber; $i++) {
+			$sleepTime = $waitStrategy->computeSleepTime($attempt);
+			$this->assertEquals($expected, $sleepTime);
+		}
+	}
+
+	function testRandomWaitMaximumGreaterThanMinimum()
+	{
+		$this->expectException(\InvalidArgumentException::class);
+
+		WaitStrategies::randomWait(1, 1);
+	}
+
+	function testRandomWaitMinimumAtLeastZero()
+	{
+		$this->expectException(\InvalidArgumentException::class);
+
+		WaitStrategies::randomWait(-1, 1);
+	}
+
+	function testRandomWait()
+	{
+		$attempt = \Mockery::mock(Attempt::class);
+
+		$minimum = 0;
+		$maximum = 1;
+		$waitStrategy = WaitStrategies::randomWait($minimum, $maximum);
+
+		$set = [];
+		for ($i = 0; $i < 1000; $i++) {
+			$sleepTime = $waitStrategy->computeSleepTime($attempt);
+			$key = strval($sleepTime);
+
+			$this->assertArrayNotHasKey($key, $set);
+			$set[$key] = $sleepTime;
+
+			$this->assertGreaterThanOrEqual($minimum, $sleepTime);
+			$this->assertLessThanOrEqual($maximum, $sleepTime);
+		}
+	}
+
+	function testIncrementingWaitValidation()
+	{
+		$this->expectException(\InvalidArgumentException::class);
+
+		WaitStrategies::incrementingWait(-1, 0);
+	}
+
+	function testIncrementingWait()
+	{
+		$attempt = \Mockery::mock(Attempt::class)
+			->shouldReceive('getAttemptNumber')
+			->andReturnValues([1, 2, 3, 4, 5])
+			->getMock();
+
+		$waitStrategy = WaitStrategies::incrementingWait(1, 2);
+
+		$series = [1, 3, 5, 7, 9];
+		while (!empty($series)) {
+			$expected = array_shift($series);
+
+			$sleepTime = $waitStrategy->computeSleepTime($attempt);
+			$this->assertEquals($expected, $sleepTime);
+		}
+	}
+
+	function testExponentialValidationMultiplierLessThanMaximumWait()
+	{
+		$this->expectException(\InvalidArgumentException::class);
+
+		WaitStrategies::exponentialWait(1, 1);
+	}
+
+	function testExponentialValidationMaximumWaitLessThanZero()
+	{
+		$this->expectException(\InvalidArgumentException::class);
+
+		WaitStrategies::exponentialWait(1, -1);
+	}
+
+	function testExponentialValidationMultiplierZero()
+	{
+		$this->expectException(\InvalidArgumentException::class);
+
+		WaitStrategies::exponentialWait(0);
+	}
+
+	function testExponentialWait()
+	{
+		$attempt = \Mockery::mock(Attempt::class)
+			->shouldReceive('getAttemptNumber')
+			->andReturnValues([1, 2, 3, 4, 5])
+			->getMock();
+
+		$waitStrategy = WaitStrategies::exponentialWait();
+
+		$series = [1, 2, 4, 8, 16];
+		while (!empty($series)) {
+			$expected = array_shift($series);
+
+			$sleepTime = $waitStrategy->computeSleepTime($attempt);
+			$this->assertEquals($expected, $sleepTime);
+		}
+	}
+
+	function testFibonacciValidationMultiplierLessThanMaximumWait()
+	{
+		$this->expectException(\InvalidArgumentException::class);
+
+		WaitStrategies::fibonacciWait(1, 1);
+	}
+
+	function testFibonacciValidationMaximumWaitLessThanZero()
+	{
+		$this->expectException(\InvalidArgumentException::class);
+
+		WaitStrategies::fibonacciWait(1, -1);
+	}
+
+	function testFibonacciValidationMultiplierZero()
+	{
+		$this->expectException(\InvalidArgumentException::class);
+
+		WaitStrategies::fibonacciWait(0);
+	}
+
+	function testFibonacciWait()
+	{
+		$attempt = \Mockery::mock(Attempt::class)
+			->shouldReceive('getAttemptNumber')
+			->andReturnValues([1, 2, 3, 4, 5])
+			->getMock();
+
+		$waitStrategy = WaitStrategies::fibonacciWait();
+
+		$fibonacci = [1, 1, 2, 3, 5];
+		while (!empty($fibonacci)) {
+			$expected = array_shift($fibonacci);
+
+			$sleepTime = $waitStrategy->computeSleepTime($attempt);
+			$this->assertEquals($expected, $sleepTime);
+		}
+	}
+
+	function testExceptionValidation()
+	{
+		$this->expectException(\InvalidArgumentException::class);
+
+		$exceptionHandler = fn() => 0;
+		WaitStrategies::exceptionWait('Foo', $exceptionHandler);
+	}
+
+	function testExceptionWait()
+	{
+		$exceptionHandler = fn(\RuntimeException $e) => 1;
+
+		$attempt = \Mockery::mock(Attempt::class)
+			->shouldReceive('hasException')
+			->andReturn(true)
+			->shouldReceive('getExceptionCause')
+			->andReturn(new \RuntimeException())
+			->getMock();
+
+		$waitStrategy = WaitStrategies::exceptionWait(\RuntimeException::class, $exceptionHandler);
+
+		$sleepTime = $waitStrategy->computeSleepTime($attempt);
+		$this->assertEquals(1, $sleepTime);
+	}
+
+	function testExceptionWaitZero()
+	{
+		$exceptionHandler = fn(\RuntimeException $e) => 1;
+
+		$attempt = \Mockery::mock(Attempt::class)
+			->shouldReceive('hasException')
+			->andReturn(true)
+			->shouldReceive('getExceptionCause')
+			->andReturn(new \Exception())
+			->getMock();
+
+		$waitStrategy = WaitStrategies::exceptionWait(\RuntimeException::class, $exceptionHandler);
+
+		$sleepTime = $waitStrategy->computeSleepTime($attempt);
+		$this->assertEquals(0, $sleepTime);
+	}
+
+	function testJoinValidation()
+	{
+		$this->expectException(\InvalidArgumentException::class);
+
+		WaitStrategies::join();
+	}
+
+	function testJoin()
+	{
+		$expected = 3;
+
+		$waitStrategy1 = \Mockery::mock(WaitStrategy::class)
+			->shouldReceive('computeSleepTime')
+			->andReturn(1)
+			->getMock();
+
+		$waitStrategy2 = \Mockery::mock(WaitStrategy::class)
+			->shouldReceive('computeSleepTime')
+			->andReturn(2)
+			->getMock();
+
+		$attempt = \Mockery::mock(Attempt::class);
+
+		$waitStrategy = WaitStrategies::join($waitStrategy1, $waitStrategy2);
+
+		$sleepTime = $waitStrategy->computeSleepTime($attempt);
+		$this->assertEquals($expected, $sleepTime);
+	}
+}
