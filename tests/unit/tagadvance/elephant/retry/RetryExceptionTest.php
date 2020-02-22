64f7559cbf35bb1396f9ecab1a297bb61678e220
Tag <tagadvance+github.com@gmail.com>
2020-02-21 21:09:18 -0700

    improve test coverage

--- tests/unit/tagadvance/elephant/retry/RetryExceptionTest.php
+++ tests/unit/tagadvance/elephant/retry/RetryExceptionTest.php
@@ -6,16 +6,55 @@ use PHPUnit\Framework\TestCase;
 
 class RetryExceptionTest extends TestCase
 {
+	function testConstructorWithMessage()
+	{
+		$expected = 'This is a message';
+
+		$exception = new RetryException($expected, 0, self::createAttempt());
+
+		$actual = $exception->getMessage();
+
+		$this->assertEquals($expected, $actual);
+	}
+
 	function testConstructorWithNullMessage()
 	{
-		$attempt = \Mockery::mock(Attempt::class)
+		$expected = 'Retrying failed to complete successfully after 0 attempts.';
+
+		$exception = new RetryException(null, 0, self::createAttempt());
+
+		$actual = $exception->getMessage();
+
+		$this->assertEquals($expected, $actual);
+	}
+
+	function testGetNumberOfFailedAttempts()
+	{
+		$expected = 0;
+
+		$exception = new RetryException('Foo', $expected, self::createAttempt());
+
+		$actual = $exception->getNumberOfFailedAttempts();
+
+		$this->assertEquals($expected, $actual);
+	}
+
+	function testGetLastFailedAttempt()
+	{
+		$expected = self::createAttempt();
+
+		$exception = new RetryException('Foo', 0, $expected);
+
+		$actual = $exception->getLastFailedAttempt();
+
+		$this->assertEquals($expected, $actual);
+	}
+
+	private static function createAttempt(): Attempt
+	{
+		return \Mockery::mock(Attempt::class)
 			->shouldReceive('getExceptionCause')
 			->andReturn(new \Exception())
 			->getMock();
-		$exception = new RetryException(null, 0, $attempt);
-
-		$expected = 'Retrying failed to complete successfully after 0 attempts.';
-		$actual = $exception->getMessage();
-		$this->assertEquals($expected, $actual);
 	}
 }
